32b1999a57f69df4048cc760433c6fb5f7027b11
Marlou Lopez <08.marlou.lopez@gmail.com>
2019-02-21 22:03:07 +0800

    Initial Commit

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+   // DB Params
+   define('DB_HOST', 'localhost');
+   define('DB_USER', 'server');
+   define('DB_PASS', '');
+   define('DB_NAME', 'tmvc');
+   // App Root
+   define('APPROOT', dirname(dirname(__FILE__)));
+   // URL Root
+   define('URLROOT', 'http://localhost/Practice/PHP-OOP-MVC');
+   // Site Name
+   define('SITENAME', 'Practice MVC');
